0690a7d5b5b6f68fdb2ba4a2d926bdd72f100c1b
Singam Ravi Babu <singam.ravibabu@gmail.com>
2026-02-04 17:28:13 +0530

    Initial commit

---
+++ project1/test_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
+def function_view_test(request):
+    return HttpResponse("<h1>Function-Based View - Siricila</h1>")
+
+class ClassViewTest(TemplateView):
+    template_name = "test.html"
