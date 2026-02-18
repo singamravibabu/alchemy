409d059578c5d6eea33fe1608bc25393017dee62
Singam Ravi Babu <singam.ravibabu@gmail.com>
2026-02-18 17:18:35 +0530

    Generic Views

--- project1/employee_learning/urls.py
+++ project1/employee_learning/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import CourseList, CourseDetail, CourseCreate, CourseUpdate, CourseDelete
 
 urlpatterns = [
-    path("course-list/", CourseList.as_view()),
+    path("course-list/", CourseList.as_view(), name="course_list"),
     path("course-detail/<int:pk>/", CourseDetail.as_view()),
-    path('course-create/', CourseCreate.as_view()),
-	path('course-update/<int:pk>/', CourseUpdate.as_view()),
-	path('course-delete/<int:pk>/', CourseDelete.as_view()),
+    path("course-create/", CourseCreate.as_view()),
+    path("course-update/<int:pk>/", CourseUpdate.as_view()),
+    path("course-delete/<int:pk>/", CourseDelete.as_view()),
 ]
--- project1/employee_learning/views.py
+++ project1/employee_learning/views.py
@@ -13,17 +13,20 @@ class CourseDetail(DetailView):
     model = LearningCourse
     template_name = "employee_learning/course_detail.html"
     context_object_name = "course"
+
 class CourseCreate(CreateView):
-	model=LearningCourse
-	template_name = 'employee_learning/course_create.html'
-	fields=('title', 'level', 'employee')
-	success_url=reverse_lazy ('course_list')
+    model = LearningCourse
+    template_name = "employee_learning/course_create.html"
+    fields = ("title", "level", "employee")
+    success_url = reverse_lazy("course_list")
+
 class CourseUpdate(UpdateView):
-	model=LearningCourse
-	template_name = 'employee_learning/course_update.html'
-	fields=('title', 'level','employee')
-	success_url=reverse_lazy ('course_list')
+    model = LearningCourse
+    template_name = "employee_learning/course_update.html"
+    field = ("title", "level", "employee")
+    success_url = reverse_lazy("course_list")
+
 class CourseDelete(DeleteView):
-	model=LearningCourse
-	template_name = 'employee_learning/course_delete.html'
-	success_url=reverse_lazy ('course_list')
+    model = LearningCourse
+    template_name = "employee_learning/course_delete.html"
+    success_url = reverse_lazy("course_list")
